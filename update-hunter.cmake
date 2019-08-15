5ca7b9b091a3d6cbb2c8abdde3d8dbde93ef2ea7
Joerg-Christian Boehme <joerg@chaosdorf.de>
2019-08-15 17:01:09 +0200

    Write all in the current directory

--- update-hunter.cmake
+++ update-hunter.cmake
@@ -4,10 +4,10 @@ execute_process(COMMAND ${CMAKE_COMMAND} -E remove -f CMakeLists.txt)
 execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory cmake)
 execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory cmake)
 
-file(DOWNLOAD https://raw.githubusercontent.com/hunter-packages/gate/master/cmake/HunterGate.cmake ${CMAKE_CURRENT_LIST_DIR}/cmake/HunterGate.cmake)
+file(DOWNLOAD https://raw.githubusercontent.com/hunter-packages/gate/master/cmake/HunterGate.cmake cmake/HunterGate.cmake)
 
-file(DOWNLOAD https://github.com/ruslo/hunter/releases/latest ${CMAKE_CURRENT_LIST_DIR}/hunterversion.txt)
-file(READ ${CMAKE_CURRENT_LIST_DIR}/hunterversion.txt HTML_CONTENT)
+file(DOWNLOAD https://github.com/ruslo/hunter/releases/latest ./hunterversion.txt)
+file(READ hunterversion.txt HTML_CONTENT)
 string(REGEX MATCH "https://github.com/ruslo/hunter/archive/v.........tar.gz" HUNTER_URL "${HTML_CONTENT}")
 string(REGEX MATCH ">\"........................................\"<" HUNTER_SHA1 "${HTML_CONTENT}")
 string(REPLACE ">\"" "" HUNTER_SHA1 "${HUNTER_SHA1}")
@@ -15,7 +15,7 @@ string(REPLACE "\"<" "" HUNTER_SHA1 "${HUNTER_SHA1}")
 message(STATUS ${HUNTER_URL})
 message(STATUS ${HUNTER_SHA1})
 
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove -f ${CMAKE_CURRENT_LIST_DIR}/hunterversion.txt)
+execute_process(COMMAND ${CMAKE_COMMAND} -E remove -f hunterversion.txt)
 
 file(WRITE CMakeLists.txt
 "cmake_minimum_required(VERSION 3.6)
